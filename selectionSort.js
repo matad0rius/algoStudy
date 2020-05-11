9ffb0a4eb77620e85c259a72ea5e04bffc9c40a1
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-05-11 12:10:15 -0300

    Add files via upload

---
+++ selectionSort.js
@@ -0,0 +1,32 @@
+// n^2 //
+
+
+// From i = 0 to < arr.length
+    // Assume the ele at 'i' is the least in the arr, assign i to 'indexOfMin'
+        // For j from i+1 to end of arr
+            // See if there is an ele with lower value
+                // If there is record its index
+    // If the index of the current ele and the index of the lowest ele isnt the same swap them
+
+    
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = 1;
+
+        for (let j = i+1; j < arr.length; j++){
+            let indexOfMin = i;
+
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+
+        if (indexOfMin !== i) {
+            let lesser = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = lesser;
+        }
+    }
+
+    return arr;
+}
