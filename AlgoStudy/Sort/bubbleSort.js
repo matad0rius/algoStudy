99878dfaa803d0682d8cd0ea7c33169ea84ee624
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-05-11 12:02:06 -0300

    Add files via upload

---
+++ AlgoStudy/Sort/bubbleSort.js
@@ -0,0 +1,21 @@
+// BubbleSort n^2 //
+
+// From i = 0 to < arr.length
+    // From j = 0 to (arr.length -i -1)
+        // If the ele at j is > j+1
+            // Swap ele at j and j+1
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length -i -1); j++) {
+
+            if (arr[j] > arr[j+1]) {
+                const lesser = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = lesser;
+            }
+        }
+    }
+
+    return arr;
+}
