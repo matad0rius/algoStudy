bcbdd874a54d2667d1bd5e8de34bab0fbee91660
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-05-11 19:30:58 -0300

    Update bubbleSort.js

--- AlgoStudy/Sort/bubbleSort.js
+++ AlgoStudy/Sort/bubbleSort.js
@@ -1,5 +1,6 @@
 // BubbleSort n^2 Quadratic time // Every pair of adjacent values is compared, and then 
 // the two values swap positions if the first value is greater than the second 
+// Iteration 1: [5,3,1,4,6] → [3,5,1,4,6] → [3,1,5,4,6] → [3,1,4,5,6] → [3,1,4,5,6]
 
 // From i = 0 to < arr.length
     // From j = 0 to (arr.length -i -1)
