3eeeb0b87cfc706c5f5cfa08cd80fa4aaa4ffadf
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-05-12 10:09:45 -0300

    Update selectionSort.js

--- AlgoStudy/Sort/selectionSort.js
+++ AlgoStudy/Sort/selectionSort.js
@@ -11,11 +11,9 @@
     
 function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
-        let indexOfMin = 1;
+        let indexOfMin = i;
 
-        for (let j = i+1; j < arr.length; j++){
-            let indexOfMin = i;
-
+        for (let j = i+1; j < arr.length; j++)
             if (arr[j] < arr[indexOfMin]) {
                 indexOfMin = j;
             }
