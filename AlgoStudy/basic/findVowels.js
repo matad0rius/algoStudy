6ac862f4d68eee438c7b886391ecafcc7d052977
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-05-16 01:02:05 -0300

    Add files via upload

---
+++ AlgoStudy/basic/findVowels.js
@@ -0,0 +1,24 @@
+// 1st
+
+function vowels(str) {
+    let count = 0;
+    const checker = "aeiou";
+
+    for (let char of str.toLowerCase()) {
+        if (checker.includes(char)) {
+            counter++
+        }
+    }
+
+    return count;
+}
+
+// 2nd
+
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi); // match founf return arr not found return null
+    return matches ? matches.length : 0;
+}
+
+
+module.exports = vowels;
