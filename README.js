c2f45dbf6ab4fa3e1e700f31b36d8117795985dd
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-05-17 00:37:54 -0300

    Update README.js

--- README.js
+++ README.js
@@ -1,15 +1,15 @@
 # algoStudy
 
 Name	        Insert	    Access	Search	Delete	    Comments
-Array	        O(n)	    O(1)	O(n)	O(n)	    Insertion to the end is O(1). 
-HashMap	        O(1)	    O(1)	O(1)	O(1)	    Rehashing might affect insertion time. 
-Map (BST)	    O(log(n))	-	    O(log(n))O(log(n))	Implemented using Binary Search Tree
-Set (HM)	    O(1)	    -	    O(1)	O(1)	    Set using a HashMap implementation. 
-Set (List)	    O(n)	    -	    O(n)	O(n)	    Implemented using Binary Search Tree
-Set (BST)	    O(log(n))	-	    O(log(n))O(log(n))	Implemented using Binary Search Tree
-LS (singly)	    O(n)	    -	    O(n)	O(n)	    Adding/Removing to the start of the list is O(1). 
-LS (doubly)	    O(n)	    -	    O(n)	O(n)	    Adding/Deleting from the beginning/end is O(1). But, deleting/adding from the middle is O(n).
-Stack (array)	O(1)	    -	    -	    O(1)	    Insert/delete is last-in, first-out (LIFO)
-Queue (naïve)	O(1)	    -	    -	    O(n)        Remove (Array.shift) is O(n)
-Queue (array	O(1)	    -	    -	    O(1)        Worst time insert is O(n). However amortized is O(1)
-Queue (list)	O(1)	    -	    -	    O(1)	    Using Doubly Linked List with reference to the last element.
+Array	        O(n)	      O(1)	  O(n)	  O(n)	      Insertion to the end is O(1). 
+HashMap	      O(1)	      O(1)	  O(1)	  O(1)	      Rehashing might affect insertion time. 
+Map (BST)	    O(log(n))	  -	      O(log(n))O(log(n))	Implemented using Binary Search Tree
+Set (HM)	    O(1)	      -	      O(1)	  O(1)	      Set using a HashMap implementation. 
+Set (List)	  O(n)	      -	      O(n)	  O(n)	      Implemented using Binary Search Tree
+Set (BST)	    O(log(n))	  -	      O(log(n))O(log(n))	Implemented using Binary Search Tree
+LS (singly)	  O(n)	      -	      O(n)	  O(n)	      Adding/Removing to the start of the list is O(1). 
+LS (doubly)	  O(n)	      -	      O(n)	  O(n)	      Adding/Deleting from the beginning/end is O(1). But, deleting/adding from the middle is O(n).
+Stack (array)	O(1)	      -	      -	      O(1)	      Insert/delete is last-in, first-out (LIFO)
+Queue (naïve)	O(1)	      -	      -	      O(n)        Remove (Array.shift) is O(n)
+Queue (array	O(1)	      -	      -	      O(1)        Worst time insert is O(n). However amortized is O(1)
+Queue (list)	O(1)	      -	      -	      O(1)	      Using Doubly Linked List with reference to the last element.
