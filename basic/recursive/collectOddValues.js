0e61beb2057f60c47ddfcd95b1729b9032a4def7
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-06-10 11:15:19 -0300

    Update collectOddValues.js

--- basic/recursive/collectOddValues.js
+++ basic/recursive/collectOddValues.js
@@ -1,3 +1,5 @@
+// Helper method recursion
+
 function collectOddValues(arr){
     
     let result = [];
@@ -20,3 +22,26 @@ function collectOddValues(arr){
 }
 
 collectOddValues([1,2,3,4,5,6,7,8,9])
+
+
+// Pure recursion
+
+function collectOddValues(arr){
+    let newArr = [];
+    
+    if(arr.length === 0) {
+        return newArr;
+    }
+        
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+        
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+collectOddValues([1,2,3,4,5])
+                                        
+                                                                
+                                 
