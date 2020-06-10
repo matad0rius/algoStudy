186dadd4319e8ccd933be597bd5efaf4c33f29b7
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-06-10 10:57:49 -0300

    Update factorial.js

--- basic/recursive/factorial.js
+++ basic/recursive/factorial.js
@@ -1,3 +1,12 @@
+// Iterative
+
+function factorial(num){
+    if(num === 1) return 1;
+    return num * factorial(num-1);
+}
+
+// Recursion
+
 function factorial(num){
     if(num === 1) return 1;
     return num * factorial(num-1);
