ec2aedf5085ce440363f5747c0bc5f6dd81eabd9
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-06-11 11:11:11 -0300

    Update README.js

--- basic/bubbleSort/betterSort/README.js
+++ basic/bubbleSort/betterSort/README.js
@@ -16,3 +16,15 @@ Merge Sort O(n log n )
     If the value in the first array is larger than the value in the second array, push the value in the 
       second array into our results and move on to the next value in the second array
     Once we exhaust one array, push in all remaining values from the other array
+    
+    
+Quicksort O(log(n))
+  Is a divide and conquer algorithm. Quicksort first divides a large array into two smaller sub-arrays
+    : the low elements and the high elements. Quicksort can then recursively sort the sub-arrays
+
+The steps are:
+  Pick an element, called a pivot, from the array.
+  Partitioning: reorder the array so that all elements with values less than the pivot come before the pivot, while all elements 
+    with values greater than the pivot come after it (equal values can go either way). After this partitioning, the pivot is in its final position. This is called the partition operation.
+  Recursively apply the above steps to the sub-array of elements with smaller values and separately to the sub-array of 
+    elements with greater values.
