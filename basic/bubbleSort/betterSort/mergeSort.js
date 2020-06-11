c02d6bb4e1f417bfd3512cd1681c05f697280136
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-06-11 00:35:52 -0300

    Create mergeSort.js

---
+++ basic/bubbleSort/betterSort/mergeSort.js
@@ -0,0 +1,29 @@
+// Recursion Fib //
+
+function merge(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+// Normal
+
+function mergeSort(left, right) {
+    const results = [];
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+
+    return [...results, ...left, ...right];
+}
