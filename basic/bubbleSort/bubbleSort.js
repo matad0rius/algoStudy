51d0e1439341b99fc853319b8aeeb27732010587
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-06-10 23:37:31 -0300

    Update bubbleSort.js

--- basic/bubbleSort/bubbleSort.js
+++ basic/bubbleSort/bubbleSort.js
@@ -14,3 +14,24 @@ function bubbleSort(arr) {
 
     return arr;
 }
+
+// Optimized
+
+  let noSwaps;
+  for (let i = 0; i < arr.length; i++){
+    noSwaps = true;
+    for(let j = 0; j < (arr.length -i -1); j++){
+        
+      if(arr[j] > arr[j+1]){
+        const lesser = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = lesser;
+        noSwaps = false;         
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+}
+
+bubbleSort([8,1,2,3,4,5,6,7]);
