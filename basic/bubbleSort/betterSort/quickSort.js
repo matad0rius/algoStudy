9e85796d728dd9936960a4e55e6234afce151246
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-06-11 11:02:44 -0300

    Update quickSort.js

--- basic/bubbleSort/betterSort/quickSort.js
+++ basic/bubbleSort/betterSort/quickSort.js
@@ -1,34 +1,52 @@
-function pivot(arr, start = 0, end = arr.length - 1) {
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-  };
+// Divide and Conquer
 
-  // We are assuming the pivot is always the first element
-  let pivot = arr[start];
-  let swapIdx = start;
+// First select an element which is to be called as pivot element.
+// Next, compare all array elements with the selected pivot element and arrange them in such a way that
+//, elements less than the pivot element are to it's left and greater than pivot is to it's right.
+// Finally, perform the same operations on left and right side elements to the pivot element.
 
-  for (let i = start + 1; i <= end; i++) {
-    if (pivot > arr[i]) {
-      swapIdx++;
-      swap(arr, swapIdx, i);
+// Code to swap two numbers in
+function swap(items, leftIndex, rightIndex) {
+    let temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+// Code to perform the partition
+function partition(items, left, right) {
+        i       = left, //left pointer
+        j       = right; //right pointer
+    let pivot   = items[Math.floor((right + left) / 2)], //middle element
+
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //swap two elements
+            i++;
+            j--;
+        }
     }
-  }
-
-  // Swap the pivot from the start the swapPoint
-  swap(arr, start, swapIdx);
-  return swapIdx;
+    return i;
 }
 
-
-function quickSort(arr, left = 0, right = arr.length -1){
-    if(left < right){
-        let pivotIndex = pivot(arr, left, right) //3
-        //left
-        quickSort(arr,left,pivotIndex-1);
-        //right
-        quickSort(arr,pivotIndex+1,right);
-      }
-     return arr;
-} 
-           
-quickSort([100,-3,2,4,6,9,1,2,5,3,23])
+// Perform the recursive operation
+// Quick sort is performed until all elements on the left array and right array are sorted.
+// Quick sort is performed on the same array and no new arrays are created in the process.
+function quickSort(items, left, right) {
+    if (items.length > 1) {
+        let index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index + 1, right);
+        }
+    }
+    return items;
+}
+// first call to quick sort
+let result = quickSort(items, 0, items.length - 1);
