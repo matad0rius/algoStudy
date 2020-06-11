127f6927e585705bb91db7eefd94eae9476d5a47
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-06-10 21:12:37 -0300

    Update naiveSearch.js

--- basic/searchAlgo/naiveSearch.js
+++ basic/searchAlgo/naiveSearch.js
@@ -1,5 +1,5 @@
 function naiveSearch(long, short){
-    var count = 0;
+    let count = 0;
     for(let i = 0; i < long.length; i++){
         for(let j = 0; j < short.length; j++){
            if(short[j] !== long[i+j]) break;
