ba3cbee2cad9dc2f95e16a0a99810cc2643084b2
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-06-13 19:32:03 -0300

    Update graphs.js

--- AlgoStudy/graphs/graphs.js
+++ AlgoStudy/graphs/graphs.js
@@ -1,5 +1,4 @@
 // undirected graph
-
 class Graph{
     constructor(){
         this.adjacencyList = {};
@@ -11,6 +10,19 @@ class Graph{
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
+    removeEdge(vertex1,vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        );
+    }
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex]
+    }
 }
-
-    
