46ef5cea12e6d88237fd3d255c9d5dc5248fa305
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-06-13 11:56:44 -0300

    Update graphs.js

--- AlgoStudy/graphs/graphs.js
+++ AlgoStudy/graphs/graphs.js
@@ -1,11 +1,16 @@
 // undirected graph
 
-class Graph {
-  constructor() {
-    this.adjacentList = {};
-  }
-  addVertex(vertex) {
-  if (!this.adjacentList[vertex]) this.adjacentList[vertex] = [];
-  }
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+    addEdge(v1,v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
 }
+
     
