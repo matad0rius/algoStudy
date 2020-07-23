034929fef2650fd6e744de3d22ab699e2719ff9d
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-07-23 09:43:16 +0200

    Create oddEvenLinkedList.js

---
+++ letcode/linkedList/oddEvenLinkedList.js
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/odd-even-linked-list/
+
+// Given a singly linked list, group all odd nodes together followed by the even nodes.
+//  Please note here we are talking about the node number and not the value in the nodes.
+
+// You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+// Example 1:
+
+// Input: 1->2->3->4->5->NULL
+// Output: 1->3->5->2->4->NULL
+
+// Example 2:
+
+// Input: 2->1->3->5->6->4->7->NULL
+// Output: 2->3->6->7->1->5->4->NULL
+
+
+// Split into 2 LS
+// 1st LS will have the odds the 2nd the evens
+// Reassign the odd nodes to only point odds nodes 
+// Reassign the last node in the 1st LS to point the first node in the 2nd LS
+// we need 4 variables oddhead (not change), evenhead (not change), odd (reasign), even (reasign)
+// so we reassign even to point odds next property and we do the same with odds 
+
+
+var oddEvenList = function(head) {
+  // Check if there is a LS
+  if (head === null) {
+    return null;
+  }
+
+  let odd = head;
+  let even = head.next;
+  let evenHead = even;
+  
+  while (even !== null && even.next !== null) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+  return head;
+};
