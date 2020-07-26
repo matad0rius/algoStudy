c533b8517eb1da275b9d6ff475ebed44cdb7c953
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-07-26 08:50:17 +0200

    Create missingNumber.js

---
+++ letcode/linkedList/easy/missingNumber.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/missing-number/
+
+// Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+// Example 1:
+
+// Input: [3,0,1]
+// Output: 2
+
+// Example 2:
+
+// Input: [9,6,4,2,3,5,7,0,1]
+// Output: 8
+
+// Note:
+// Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
+
+
+var missingNumber = function(nums) {
+    let sumIfNoneMissing = nums.length;
+    let actualSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+      sumIfNoneMissing = sumIfNoneMissing + i;
+      actualSum = actualSum + nums[i]
+    }
+
+    return sumIfNoneMissing - actualSum;
+};
