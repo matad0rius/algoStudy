13e9202deac559cda71a958a2ba1646a0e93646b
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-08-09 11:32:26 +0200

    Create pascalTriangle1.js

---
+++ letcode/linkedList/easy/pascalTriangle1.js
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/pascals-triangle/
+
+// Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+// Example:
+
+// Input: 5
+// Output:
+// [
+//      [1],
+//     [1,1],
+//    [1,2,1],
+//   [1,3,3,1],
+//  [1,4,6,4,1]
+// ]
+
+var generate = function(numRows) {
+    let triangle = [];
+
+    if (numRows === 0) {
+        return triangle;
+    }
+
+    triangle.push([1]);
+
+    for (let i = 1; i < numRows; i++) {
+        let prevRow = triangle[i - 1];
+        let newRow = [];
+
+        newRow.push(1);
+
+        for (let j = 1; j < prevRow.length; j++) {
+            newRow.push(prevRow[j - 1]+ prevRow[j]);
+        }
+
+        newRow.push(1);
+        triangle.push(newRow);
+    }
+
+    return triangle;
+};
