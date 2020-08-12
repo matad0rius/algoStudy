1677f3c00e162f3182594e113e4555036a809a41
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2020-08-12 12:54:26 +0200

    Create twoSumIndex.js

---
+++ letcode/linkedList/easy/twoSumIndex.js
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+// Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+// Note:
+
+//     Your returned answers (both index1 and index2) are not zero-based.
+//     You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+// Example:
+
+// Input: numbers = [2,7,11,15], target = 9
+// Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+
+var twoSum = function(numbers, target) {
+    let leftPointer = 0;
+    let rightPointer = numbers.length - 1;
+
+    let result  = [];
+
+    while (leftPointer < rightPointer) {
+        let leftEle = numbers[leftPointer];
+        let rightEle = numbers[rightPointer];
+
+        if (leftEle + rightEle === target) {
+            result.push(leftPointer + 1);
+            result.push(rightPointer + 1);
+            break;
+        } else if (leftEle + rightEle > target) {
+            rightPointer--;
+        } else {
+            leftPointer++;
+        }
+    }
+
+    return result;
+};
